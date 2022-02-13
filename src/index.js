f6aaae4bd09e0dc8a844693b761503b9c9a3656c
Sonali <91842268+Sonalisatyarthy@users.noreply.github.com>
2022-02-13 20:23:21 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './screens/Home/Home';
+
+ReactDOM.render(
+  <Home/>,
+  document.getElementById('root')
+);            
